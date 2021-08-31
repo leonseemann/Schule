d0074e88bfa18e49f97adc3fa2d8d8125b394503
slowede <79045490+slowede@users.noreply.github.com>
2021-08-31 12:42:14 +0200

    init

---
+++ SFT König/Rechteck/Rechteck.java
@@ -0,0 +1,17 @@
+class Rechteck {
+    private double laenge;
+    private double breite;
+    private double flaeche;
+
+    public void setLaenge(double l){
+        laenge = l;
+    }
+
+    public void setBreite(double b){
+        breite = b; 
+    }
+
+    public double rechneFlaeche(){
+        flaeche = laenge * breite;
+    }
+}
---
+++ SFT König/Rechteck/Start.java
@@ -0,0 +1,8 @@
+class Start {
+    public static voud main(String[]arg){
+        Rechteck rechteck = new Rechteck();
+        rechteck.setLaenge(3.2);
+        rechteck.setBreite(1.3);
+        System.out.println(rechteck.rechneFlaeche());
+    }
+}
