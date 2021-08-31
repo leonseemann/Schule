0709d92f4f23bda67e36af935565d88cf3f49673
slowede <79045490+slowede@users.noreply.github.com>
2021-08-31 12:28:09 +0200

    init

---
+++ SFT König/Person/Person.java
@@ -0,0 +1,12 @@
+class Person{
+    private String name;
+    private String vorname;
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ SFT König/Person/Start.java
@@ -0,0 +1,7 @@
+class Start {
+    public static void main(String[]arg){
+        Person peter = new Person();
+        peter.setName("Peter");
+        System.out.println(peter.getName());
+    }    
+}
