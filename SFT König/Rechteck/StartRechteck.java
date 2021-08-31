dd2997b11d7d0c5afb6de66e947f1a5b603482fa
slowede <79045490+slowede@users.noreply.github.com>
2021-08-31 21:57:36 +0200

    init

--- SFT König/Rechteck/StartRechteck.java
+++ SFT König/Rechteck/StartRechteck.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 class StartRechteck {
     public static void main(String[]arg){
-        Scanner myObj = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         Rechteck rechteck = new Rechteck();
 
-        System.out.println("Eingabe Laenge: ");
-        double l = myObj.nextDouble();
+        System.out.println("Eingabe L\u00e4nge(cm): ");
+        double l = input.nextDouble();
         rechteck.setLaenge(l);
 
-        System.out.println("Eingabe Breite: ");
-        double b = myObj.nextDouble();
+        System.out.println("Eingabe Breite(cm): ");
+        double b = input.nextDouble();
         rechteck.setBreite(b);
 
-        System.out.println("Länge: " + l);
-        System.out.println("Breite: " + b);
-        System.out.println("Fläche: " + rechteck.rechneFlaeche());
+        System.out.print("\033[H\033[2J"); 
+
+        System.out.println("L\u00e4nge: " + l + " cm");
+        System.out.println("Breite: " + b + " cm");
+        System.out.println("Fl\u00e4che: " + rechteck.rechneFlaeche() + " cm^2");
     }
 }
