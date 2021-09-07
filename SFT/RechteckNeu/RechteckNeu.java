e8a086a01b36da2775c60c1a322755fa4e885297
slowede <79045490+slowede@users.noreply.github.com>
2021-09-07 12:22:05 +0200

    RechteckNeu

---
+++ SFT/RechteckNeu/RechteckNeu.java
@@ -0,0 +1,19 @@
+class RechteckNeu {
+    private double laenge;
+    private double breite;
+
+    public RechteckNeu() {
+        laenge = 1;
+        breite = 1;
+    }
+
+    public RechteckNeu(double laenge, double breite){
+        this.laenge = laenge;
+        this.breite = breite;
+    }
+
+    public double rechneFlaeche() {
+        double flaeche = laenge * breite;
+        return flaeche;
+    }
+}
---
+++ SFT/RechteckNeu/StartRechteckNeu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class StartRechteckNeu {
+    public static void main(String[] arg) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Eingabe L\u00e4nge(cm): ");
+        double l = input.nextDouble();
+
+        System.out.println("Eingabe Breite(cm): ");
+        double b = input.nextDouble();
+
+        RechteckNeu rechteck = new RechteckNeu(l, b);
+        
+        System.out.print("\033\143");
+
+        System.out.println("L\u00e4nge: " + l + " cm");
+        System.out.println("Breite: " + b + " cm");
+        System.out.println("Fl\u00e4che: " + rechteck.rechneFlaeche() + " cm^2");
+    }
+}
