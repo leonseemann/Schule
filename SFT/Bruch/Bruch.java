175600b894f2aeeac3060d3591bd409f69b0fd2c
slowede <79045490+slowede@users.noreply.github.com>
2021-09-28 12:16:07 +0200

    java_bruch

---
+++ SFT/Bruch/Bruch.java
@@ -0,0 +1,30 @@
+class Bruch {
+    private int zaehler;
+    private int nenner;
+
+    public Bruch() {
+        this(3);
+    }
+
+    public Bruch(int z) {
+        this(z, 1);
+    }
+
+    public Bruch(int z, int n) {
+        if (n <= 0) {
+            n = 1;
+        }
+
+        zaehler = z;
+        nenner = n;
+    }
+
+    public double ausgabeDezimal() {
+        double dezimal = zaehler / nenner;
+        return dezimal;
+    }
+
+    public void ausgabeBruch() {
+        System.out.println(zaehler + "/" + nenner);
+    }
+}
---
+++ SFT/Bruch/StartBruch.java
@@ -0,0 +1,7 @@
+class StartBruch {
+    public static void main(String[] arg) {
+        Bruch bruch = new Bruch(2, 3);
+        System.out.println(bruch.ausgabeDezimal());
+        bruch.ausgabeBruch();
+    }
+}
