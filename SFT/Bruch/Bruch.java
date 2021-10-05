4e454ad90f3c665430d3bf5dd455021c5f3f3f12
slowede <79045490+slowede@users.noreply.github.com>
2021-10-05 12:27:12 +0200

    java

--- SFT/Bruch/Bruch.java
+++ SFT/Bruch/Bruch.java
@@ -31,12 +31,31 @@ class Bruch {
     public Bruch addition(Bruch b) {
         Bruch ausgabeB = new Bruch();
 
-        int z = this.zaehler * b.zaehler + b.nenner * this.nenner;
+        ausgabeB.zaehler = this.zaehler * b.zaehler + b.nenner * this.nenner;
 
-        int n = this.nenner * b.nenner;
+        ausgabeB.nenner = this.nenner * b.nenner;
 
-        ausgabeB.zaehler = z;
-        ausgabeB.nenner = n;
+        return ausgabeB;
+    }
+
+    public Bruch subtraktion(Bruch b) {
+        Bruch ausgabeB = new Bruch();
+
+        ausgabeB.zaehler = this.zaehler * b.zaehler - b.nenner * this.nenner;
+
+        ausgabeB.nenner = this.nenner * b.nenner;
+
+        return ausgabeB;
+    }
+
+    public Bruch multiplikation(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.zaehler, this.nenner * b.nenner);
+
+        return ausgabeB;
+    }
+
+    public Bruch dividieren(Bruch b) {
+        Bruch ausgabeB = new Bruch(this.zaehler * b.nenner, this.nenner * b.zaehler);
 
         return ausgabeB;
     }
--- SFT/Bruch/StartBruch.java
+++ SFT/Bruch/StartBruch.java
@@ -3,5 +3,11 @@ class StartBruch {
         Bruch bruch = new Bruch(3, 5);
         System.out.println(bruch.ausgabeDezimal());
         bruch.ausgabeBruch();
+
+        System.out.println("--------------------------------------------------------");
+
+        Bruch brech = new Bruch(4, 5);
+        System.out.println(brech.ausgabeDezimal());
+        brech.ausgabeBruch();
     }
 }
