6085030e89772f0c1d3b3394cd9af63191b3406c
slowede <79045490+slowede@users.noreply.github.com>
2021-10-26 10:28:14 +0200

    java

--- SFT/Bruch/StartBruch.java
+++ SFT/Bruch/StartBruch.java
@@ -1,23 +1,9 @@
 class StartBruch {
     public static void main(String[] arg) {
         Bruch bruch = new Bruch(3, 5);
-        System.out.println(bruch.ausgabeDezimal());
-        bruch.ausgabeBruch();
-
-        System.out.println("--------------------------------------------------------");
-
         Bruch brech = new Bruch(4, 5);
-        System.out.println(brech.ausgabeDezimal());
-        brech.ausgabeBruch();
-
-        System.out.println("--------------------------------------------------------");
-
         Bruch g = new Bruch(4, 7);
-        System.out.println(brech.ausgabeDezimal());
-        brech.ausgabeBruch();
-
-        System.out.println("--------------------------------------------------------");
-
+        
         g = bruch.addition(brech);
     }
 }
